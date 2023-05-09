d0dfd672c94d4718adee92b3cb1ee0bd2cb4f87b
Unknown <29576603+teohsen@users.noreply.github.com>
2023-05-09 21:51:30 +0800

    Update 54. Spiral Matrix.py
    Using List removes the need to *zip

--- leetcode/54. Spiral Matrix.py
+++ leetcode/54. Spiral Matrix.py
@@ -11,15 +11,15 @@ class Solution:
         # matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
         # matrix = [[2, 3]]
         # matrix = [[2], [3]]
-        # matrix = [[1,2,3,4],
-        #           [5,6,7,8],
-        #           [9,10,11,12],
-        #           [13,14,15,16]]
+        matrix = [[1,2,3,4],
+                  [5,6,7,8],
+                  [9,10,11,12],
+                  [13,14,15,16]]
 
         res = []
         while matrix:
             res.extend(matrix.pop(0))
-            matrix = [*zip(*matrix)][::-1]  # Rotate anti-clockwise 90 degree
+            matrix = list(zip(*matrix))[::-1] # Rotate anti-clockwise 90 degree
             # Reference: https://medium.com/@SantalTech/a-clever-one-liner-to-rotate-2d-arrays-in-python-f67608ec77f9
             # list(zip(*matrix[::-1])) <- Rotate clockwise 90 degree
 
