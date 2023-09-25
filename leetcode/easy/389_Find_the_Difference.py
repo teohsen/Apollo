9bdc15d63abdbe408b47647d7903bdab00c1f7ab
UnknownUdon <29576603+teohsen@users.noreply.github.com>
2023-09-25 20:34:11 +0800

    Update 389_Find_the_Difference.py

--- leetcode/easy/389_Find_the_Difference.py
+++ leetcode/easy/389_Find_the_Difference.py
@@ -9,11 +9,33 @@ class Solution:
 
         return list((Counter(t) - Counter(s)).keys())[0]
 
+    def solution_2(self, s, t):
+        """
+        unicode method
+
+        :param s:
+        :param t:
+        :return:
+        """
+        sum_s = 0
+        sum_t = 0
+
+        for _ in s:
+            sum_s += ord(_)
+
+        for _ in t:
+            sum_t += ord(_)
+
+        return chr(sum_t - sum_s)
+
     def test(self):
         s = "abcd"
         t = "abcde"
 
         result = self.findTheDifference(s, t)
+        print(result)
+
+        result = self.solution_2(s, t)
         print(result)
 
 
